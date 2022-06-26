bb2efbe54b17ccb8e139ceb3eae79a4f7d460ec3
gajulaharipriya <107806081+gajulaharipriya@users.noreply.github.com>
2022-06-26 20:15:15 +0530

    Add files via upload

---
+++ AdminDepartment.java
@@ -0,0 +1,15 @@
+package com.hari.model;
+
+public class AdminDepartment extends SuperDepartment {
+	public String departmentName() {
+		return("Welcome to Admin Department");
+		}
+
+		public String getTodaysWork() {
+			return("complete your documents Submission");
+		}
+		public String getWorkDeadline() {
+			return("complete by EOD");
+		}
+	
+}
